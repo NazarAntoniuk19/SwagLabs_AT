96663ea4d9cadc6cd4ea28e236bd552313edb9ac
Nazar Antoniuk <antnazvol@gmail.com>
2025-02-08 21:47:43 +0200

    update test-case

--- test/specs/social-media-footer-test-case.js
+++ test/specs/social-media-footer-test-case.js
@@ -6,24 +6,19 @@ async function returnPage() {
   await browser.url("https://www.saucedemo.com/inventory.html");
 }
 
-async function homePage() {
-  const inventoryContainer = await $("#inventory_container");
-  await expect(inventoryContainer).toBeDisplayed();
-}
-
 describe("Footer functionality", () => {
   it("should open social networks from the footer", async () => {
     await loginPage.open();
     await loginPage.login("standard_user", "secret_sauce");
-    await homePage();
+    await menuPage.isInventoryDisplayed();
     await menuPage.clickTwitterLink();
     await returnPage();
-    await homePage();
+    await menuPage.isInventoryDisplayed();
     await menuPage.clickFacebookLink();
     await returnPage();
-    await homePage();
+    await menuPage.isInventoryDisplayed();
     await menuPage.clickLinkedInLink();
     await returnPage();
-    await homePage();
+    await menuPage.isInventoryDisplayed();
   });
 });
